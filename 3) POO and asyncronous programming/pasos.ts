f16a1ecde3aef01c80a7326ea3667fa952394f75
Federico Irarrazaval <fedeirar@gmail.com>
2023-04-04 10:23:10 -0300

    third course. base project

---
+++ 3) POO and asyncronous programming/pasos.ts
@@ -0,0 +1,11 @@
+/*
+!PASOS:
+*1) npm init -y
+*2) npm i typescript -D
+*3) npx tsc --init
+*4) npm i ts-node -D
+*5) gitignore
+*6) editorconfig
+*7) crear carpetas dist y src
+*8) en tsconfig.json: "outDir": "./dist",
+*/
